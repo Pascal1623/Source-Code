09cd2aff01bd4e9a72089c394b750aff577cb0e1
Pascal1623 <37091591+Pascal1623@users.noreply.github.com>
2019-04-29 20:36:14 +0900

    Create 출력.java

---
+++ JAVA 객체지향프로그래밍/음식값계산 4개의 소스코드/출력.java
@@ -0,0 +1,12 @@
+public class 출력 {
+
+    public void 메뉴() {
+        System.out.println("짜장면은 5000원");
+        System.out.println("짬뽕은 6000원");
+        System.out.println("탕수육은 15000원");
+        System.out.println("볶음밥은 7000원");
+        System.out.println("짜장밥은 7000원");
+        System.out.println(" ");
+    }
+
+}
